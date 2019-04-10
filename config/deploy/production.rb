00076586b46cda71c6d6a776869261aa66100968
ryom0624 <ryo.m0624@gmail.com>
2019-04-10 11:36:31 +0900

    rails env production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
+set :raisl_env, "production"
 set :migration_role, "db"
 server "35.243.82.203", user: "kusanagi", roles: %w{app web db instance1},
   ssh_options: {
